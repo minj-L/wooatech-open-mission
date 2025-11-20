041214c77ab6a5ee140f55674a849b6d3374f31c
minj-L <wonder_minji@naver.com>
2025-11-21 02:32:36 +0900

    style : 띄어쓰기 등 코드 포멧 수정

--- src/main/java/stock/controller/GreetingController.java
+++ src/main/java/stock/controller/GreetingController.java
@@ -9,10 +9,13 @@ import stock.domain.HelloMessage;
 
 @Controller
 public class GreetingController {
+
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
         Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        return new Greeting(
+                "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!"
+        );
     }
 }
